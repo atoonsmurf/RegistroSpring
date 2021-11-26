cff9bb3b5436e68589e241b100945ce5bb86b3b6
atoonft <atoonft@gmail.com>
2021-11-25 21:53:41 -0500

    Inicio

---
+++ src/main/java/org/ciberclass/DawiCl3SandovalSebastianApplication.java
@@ -0,0 +1,13 @@
+package org.ciberclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiCl3SandovalSebastianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiCl3SandovalSebastianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ciberclass/DawiCl3SandovalSebastianApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ciberclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiCl3SandovalSebastianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
